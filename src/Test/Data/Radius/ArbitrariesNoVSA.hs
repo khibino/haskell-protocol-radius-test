9013a3113a311b1b2fa6aa8e6b6e1ce0d3b752a9
Kei Hibino <ex8k.hibino@gmail.com>
2018-02-15 22:16:38 +0900

    add arbitraries for bottom-VSA.

---
+++ src/Test/Data/Radius/ArbitrariesNoVSA.hs
@@ -0,0 +1,63 @@
+{-# LANGUAGE EmptyCase #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+module Test.Data.Radius.ArbitrariesNoVSA () where
+
+import Test.QuickCheck (Arbitrary (..), oneof, elements)
+
+import Test.Data.Radius.ArbitrariesBase
+  (genSizedString, genAtText, genAtString)
+
+import Control.Applicative ((<$>), (<*>))
+import qualified Data.Set as Set
+
+import Data.Radius.Scalar
+  (AtText (..), AtString (..), AtInteger (..))
+import Data.Radius.Attribute
+  (NumberAbstract (..), Attribute' (..), Attribute (..),
+   numbersText, numbersString, numbersInteger)
+
+
+data EmptyVSA
+
+instance Eq EmptyVSA where
+  x == _  =  case x of {}
+
+instance Ord EmptyVSA where
+  x `compare` _  =  case x of {}
+
+
+instance Arbitrary (NumberAbstract EmptyVSA) where
+  arbitrary = oneof [ Standard <$> arbitrary ]
+
+instance Arbitrary (Attribute' EmptyVSA) where
+  arbitrary =
+    Attribute' . Standard <$> arbitrary <*> genSizedString (255 - 1 - 1)
+
+
+instance Arbitrary (Attribute EmptyVSA AtText) where
+  arbitrary =
+    Attribute
+    <$> elements (Set.toList numbersText)
+    <*> genAtText (255 - 1 - 1)
+
+instance Arbitrary (Attribute EmptyVSA AtString) where
+  arbitrary =
+    Attribute
+    <$> elements (Set.toList numbersString)
+    <*> genAtString (255 - 1 - 1)
+
+instance Arbitrary (Attribute EmptyVSA AtInteger) where
+  arbitrary =
+    Attribute
+    <$> elements (Set.toList numbersInteger)
+    <*> arbitrary
+
+{-
+-- attributeNumbersIpV4 is empty list
+instance Arbitrary (Attribute AtIpV4) where
+  arbitrary =
+    Attribute
+    <$> elements (Set.toList attributeNumbersIpV4)
+    <*> arbitrary
+ -}
